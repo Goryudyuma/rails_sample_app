7eb126fa752267c4472774f8e642d4f75b205300
Goryudyuma <umaretekyoumade@gmail.com>
2016-02-18 09:01:38 +0900

    add login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,28 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  get 'sessions/new'
+
+  root		'static_pages#home'
 
 #  get 'static_pages/home'
 
 #  get 'static_pages/help'
-  get 'help'	=>	'static_pages#help'
+  get		'help'		=>	'static_pages#help'
 
 #  get 'static_pages/about'
-  get 'about'	=>	'static_pages#about'
+  get		'about'		=>	'static_pages#about'
 
 #  get 'static_pages/contact'
-  get 'contact'	=>	'static_pages#contact'
+  get		'contact'	=>	'static_pages#contact'
 
 #  get 'users/new'
-  get 'signup'	=>	'users#new'
+  get		'signup'	=>	'users#new'
+
+  get		'login' 	=>	'sessions#new'
+
+  post		'login'		=>	'sessions#create'
+
+  delete	'logout'	=>	'sessions#destroy'
 
   resources :users
 
